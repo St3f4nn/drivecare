253bed2eeff5cc6d7848127c2142d98fd2f98690
Stefan <gajicstefan414@gmail.com>
2024-03-22 22:36:36 +0100

    Create a HERO SECTION component - style the component according to the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,20 @@ module.exports = {
   content: ["./dist/**/*.html"],
   theme: {
     extend: {
+      backgroundImage: {
+        // Gradients
+
+        overlay:
+          "linear-gradient(90deg, rgba(17, 24, 39, 1) 0%, rgba(17, 24, 39, 0.75) 25%, rgba(17, 24, 39, 0.5) 50%, rgba(17, 24, 39, 0.25) 75%, rgba(17, 24,39, 0) 100%)",
+
+        // Images
+
+        carmechanicworking:
+          "url('/dist/assets/images/car_mechanic_working.jpg')",
+      },
+      boxShadow: {
+        ctabtn: "inset 0 0 0 0.125rem #f3f4f6",
+      },
       fontFamily: {
         robotocondensed: "'Roboto Condensed', sans-serif",
       },
