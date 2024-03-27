52ce9f7a12010f39de5bc961c51c1e8d3777a26f
Stefan <gajicstefan414@gmail.com>
2024-03-27 16:52:41 +0100

    Create a FOOTER component - add current-year functionality

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,6 +5,12 @@
 const headerLowerEl = document.querySelector(".header-lower");
 const toggleMenuBtn = document.querySelector("#toggle-menu-btn");
 
+const yearEl = document.querySelector("#year");
+
+const currentYear = new Date().getFullYear();
+
+yearEl.textContent = currentYear;
+
 // Event listeners
 
 toggleMenuBtn.addEventListener("click", function () {
