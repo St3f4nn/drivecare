29d7254da746ebf136517b4ff84eaf9c2b9d9c53
Stefan <gajicstefan414@gmail.com>
2024-03-27 14:01:51 +0100

    Create a GALLERY SECTION component - style the component according to the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,23 @@ module.exports = {
           "url('../assets/images/car_mechanic_working.jpg')",
         "car-engine": "url('../assets/images/car_engine.jpg')",
         "car-in-garage": "url('../assets/images/car_in_garage.jpg')",
+        "placing-a-felugue": "url('../assets/images/placing_a_felugue.jpg')",
+        "car-mechanic-working-2":
+          "url('../assets/images/car_mechanic_working_2.jpg')",
+        "disassembled-audi": "url('../assets/images/disassembled_audi.jpg')",
+        "car-mechanic-working-3":
+          "url('../assets/images/car_mechanic_working_3.jpg')",
+        "mechanic-fills-the-oil":
+          "url('../assets/images/mechanic_fills_the_oil.jpg')",
       },
       boxShadow: {
         "cta-btn": "inset 0 0 0 0.125rem #f3f4f6",
       },
       fontFamily: {
         "roboto-condensed": "'Roboto Condensed', sans-serif",
+      },
+      scale: {
+        115: "1.15",
       },
     },
   },
